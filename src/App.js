1ea5d9aebd3f715eeb2757d383e14dbd4c6756b2
Namithact <namithact@hotmail.com>
2025-05-01 16:49:13 +0400

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import AddExpensePage from "./Pages/AddExpensePage";
+import { Suspense,lazy } from "react";
 import Footer from "./components/Footer";
-import ErrorPage from "./Pages/ErrorPage";
 function App() {
+  const HomePage =lazy(()=> import("./Pages/HomePage"));
+  const AddExpensePage=lazy(()=> import("./Pages/AddExpensePage"));
+  const ErrorPage=lazy(()=> import("./Pages/ErrorPage"));
   return (
+    <Suspense fallback={ <div className="flex justify-center items-center h-screen">
+      {/* Tailwind CSS Spinner */}
+      <div className="border-4 border-t-4 border-blue-500 border-solid rounded-full w-16 h-16 animate-spin"></div>
+    </div>}>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -14,6 +19,7 @@ function App() {
       </Routes>
       <Footer />
     </Router>
+    </Suspense>
   );
 }
 
